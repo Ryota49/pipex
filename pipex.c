c710d11905fce4e73cf6fd481a024540c98f5345
Jean Monthioux <jemonthi@3B1.42angouleme.fr>
2026-01-14 00:28:24 +0100

    pipex norminette full OK

---
+++ pipex.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jemonthi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 19:38:58 by jemonthi          #+#    #+#             */
+/*   Updated: 2026/01/14 00:22:42 by jemonthi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	check_existing_file(char **argv)
+{
+	if (access(argv[1], F_OK) != 0)
+	{
+		write(2, "Infile doesn't exist\n", 22);
+		exit (1);
+	}
+	if (access(argv[4], F_OK) != 0)
+	{
+		write(2, "Outfile doesn't exist\n", 23);
+		exit (1);
+	}
+}
+
+void	check_existing_command_line(char **argv, char **envp)
+{
+	char	**command_line;
+	char	**all_path;
+	char	*path;
+
+	command_line = ft_split(argv[2], ' ');
+	path = find_path(envp);
+	if (!path)
+	{
+		write(2, "No path found\n", 15);
+		exit(1);
+	}
+	all_path = ft_split(path, ':');
+	join_and_execve(all_path, command_line, envp, path);
+}
+
+void	join_and_execve(char **all_path, char **cmd, char **envp, char *path)
+{
+	char	*tmp;
+	int		i;
+	int		id;
+
+	id = fork();
+	i = 0;
+	while (all_path[i])
+	{
+		tmp = ft_strjoin(all_path[i], "/");
+		path = ft_strjoin(tmp, cmd[0]);
+		free (tmp);
+		if (id == 0 && (access(path, X_OK) == 0))
+			execve(path, cmd, envp);
+		else
+			free (path);
+		i++;
+	}
+	wait(&id);
+}
+
+char	*find_path(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			return (envp[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc != 5)
+		return (0);
+	else
+	{
+		check_existing_file(argv);
+		check_existing_command_line(argv, envp);
+	}
+	return (0);
+}
--- pipex.h
+++ pipex.h
@@ -1,32 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jemonthi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 19:38:58 by jemonthi          #+#    #+#             */
+/*   Updated: 2026/01/14 00:24:59 by jemonthi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
+# include <sys/types.h>
+# include <sys/wait.h>
 
 /* creating file to test*/
 
-int test2(void);
-int test(void);
+int		test2(void);
+int		test(void);
 
 /* parsing function */
 
 void	check_existing_file(char **argv);
-void    check_existing_command_line(char **argv, char **envp);
-char    *find_path(char **envp);
-void    join_and_execve(char **all_path, char **command_line, char **envp, char *path);
+void	check_existing_command_line(char **argv, char **envp);
+char	*find_path(char **envp);
+void	join_and_execve(char **all_path, char **cmd, char **envp, char *path);
 
 /* utilities functions */
 
 char	**ft_split(char const *s, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
-
 
 #endif
